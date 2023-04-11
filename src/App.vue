0fecca5f169e392997e5a7e331b5bbde98e1c446
yanqi17118357 <3224848830@qq.com>
2023-04-11 21:21:13 +0800

    选择器选项

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
-<!--    true-value 和 false-value 是 Vue 特有的 attributes-->
-    <input
-        type="checkbox"
-        v-model="toggle"
-        :true-value="dynamicTrueValue"
-        :false-value="dynamicFalseValue" />
-<!--    复选框未选中是不会向表单提交数据的，单选框会-->
-    <input type="radio" v-model="pick" :value="first" />
-    <input type="radio" v-model="pick" :value="second" />
+    <select v-model="selected">
+        <!-- 内联对象字面量 -->
+<!--        selected 会被设为该对象字面量值 { number: 123 }-->
+        <option :value="{ number: 123 }">123</option>
+    </select>
+
+
 
 </template>
 
@@ -15,12 +13,7 @@
 export default {
     data() {
         return {
-            toggle: '',
-            dynamicTrueValue: "正确的",
-            dynamicFalseValue: "错误的",
-            pick: '',
-            first: "第一",
-            second: "第二"
+            selected: ''
         }
     }
 }
