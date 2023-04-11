f5813f7b0f8cd229dcdf416eb7d7463ccdddf0d6
yanqi17118357 <3224848830@qq.com>
2023-04-11 18:53:09 +0800

    内联事件处理器中访问事件参数 $even 或 内联箭头函数

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    data() {
+        return {
+            name: 'Vue.js'
+        }
+    },
+    methods: {
+        warn(message, event) {
+            // 这里可以访问 DOM 原生事件
+            if (event) {
+                event.preventDefault()
+            }
+            alert(message)
+        }
+    }
+}
+</script>
+
+<template>
+    <!-- 使用特殊的 $event 变量 -->
+    <button @click="warn('Form cannot be submitted yet.', $event)">
+        Submit
+    </button>
+
+    <!-- 使用内联箭头函数 -->
+    <button @click="(event) => warn('Form cannot be submitted yet.', event)">
+        Submit
+    </button>
+
+
+</template>
+
+<style scoped>
+
+
+</style>
