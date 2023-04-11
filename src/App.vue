5a0707b2bb2f43f70d2533ad4437ca325f996d1f
yanqi17118357 <3224848830@qq.com>
2023-04-11 19:17:09 +0800

    v-model 初体验

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,9 @@
 <template>
-    <!-- 单击事件将停止传递 -->
-    <a @click.stop="doThis"></a>
+    <input
+        :value="text"
+        @input="event => text = event.target.value">
 
-    <!-- 提交事件将不再重新加载页面 -->
-    <form @submit.prevent="onSubmit"></form>
-
-    <!-- 修饰语可以使用链式书写 -->
-    <a @click.stop.prevent="doThat"></a>
-
-    <!-- 也可以只有修饰符 -->
-    <form @submit.prevent></form>
-
-    <!-- 仅当 event.target 是元素本身时才会触发事件处理器 -->
-    <!-- 例如：事件处理器不来自子元素 -->
-    <div @click.self="doThat">...</div>
-
-    <!-- 添加事件监听器时，使用 `capture` 捕获模式 -->
-    <!-- 例如：指向内部元素的事件，在被内部元素处理前，先被外部处理 -->
-    <div @click.capture="doThis">...</div>
-
-    <!-- 点击事件最多被触发一次 -->
-    <a @click.once="doThis"></a>
-
-    <!-- 滚动事件的默认行为 (scrolling) 将立即发生而非等待 `onScroll` 完成 -->
-    <!-- 以防其中包含 `event.preventDefault()` -->
-    <div @scroll.passive="onScroll">...</div>
-
+    <input v-model="text">
 
 </template>
 
@@ -33,7 +11,8 @@
 export default {
     data() {
         return {
-            name: 'Vue.js'
+            name: 'Vue.js',
+            text: 'hello'
         }
     },
     methods: {
