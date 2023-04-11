68ec193e5f13389ba0187549f5e998729e67aed8
yanqi17118357 <3224848830@qq.com>
2023-04-11 21:25:49 +0800

    v-model修饰符.lazy .number .trim

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,14 @@
 <!--        selected 会被设为该对象字面量值 { number: 123 }-->
         <option :value="{ number: 123 }">123</option>
     </select>
+    <!-- 在 "change" 事件后同步更新而不是 "input" -->
+    <input v-model.lazy="msg" />
 
+<!--    输入自动转换为数字，如果该值无法被 parseFloat() 处理，那么将返回原始值。-->
+    <input v-model.number="age" />
 
+<!--    默认自动去除用户输入内容中两端的空格-->
+    <input v-model.trim="msg" />
 
 </template>
 
@@ -13,7 +19,9 @@
 export default {
     data() {
         return {
-            selected: ''
+            selected: '',
+            msg: '',
+            age: ''
         }
     }
 }
