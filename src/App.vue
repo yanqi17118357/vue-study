c5c89163bed717334c459f8a6ab54fbc459e6de2
yanqi17118357 <3224848830@qq.com>
2023-04-11 19:06:25 +0800

    事件修饰符

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,34 @@
+<template>
+    <!-- 单击事件将停止传递 -->
+    <a @click.stop="doThis"></a>
+
+    <!-- 提交事件将不再重新加载页面 -->
+    <form @submit.prevent="onSubmit"></form>
+
+    <!-- 修饰语可以使用链式书写 -->
+    <a @click.stop.prevent="doThat"></a>
+
+    <!-- 也可以只有修饰符 -->
+    <form @submit.prevent></form>
+
+    <!-- 仅当 event.target 是元素本身时才会触发事件处理器 -->
+    <!-- 例如：事件处理器不来自子元素 -->
+    <div @click.self="doThat">...</div>
+
+    <!-- 添加事件监听器时，使用 `capture` 捕获模式 -->
+    <!-- 例如：指向内部元素的事件，在被内部元素处理前，先被外部处理 -->
+    <div @click.capture="doThis">...</div>
+
+    <!-- 点击事件最多被触发一次 -->
+    <a @click.once="doThis"></a>
+
+    <!-- 滚动事件的默认行为 (scrolling) 将立即发生而非等待 `onScroll` 完成 -->
+    <!-- 以防其中包含 `event.preventDefault()` -->
+    <div @scroll.passive="onScroll">...</div>
+
+
+</template>
+
 <script>
 export default {
     data() {
@@ -17,21 +48,5 @@ export default {
 }
 </script>
 
-<template>
-    <!-- 使用特殊的 $event 变量 -->
-    <button @click="warn('Form cannot be submitted yet.', $event)">
-        Submit
-    </button>
-
-    <!-- 使用内联箭头函数 -->
-    <button @click="(event) => warn('Form cannot be submitted yet.', event)">
-        Submit
-    </button>
-
-
-</template>
-
 <style scoped>
-
-
 </style>
