68834a6a2b9c0c0ef197922cd73a2013095c729c
yanqi17118357 <3224848830@qq.com>
2023-04-11 19:28:48 +0800

    v-model 复选框

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-    <p>Message is: {{ message }}</p>
-    <input v-model="message" placeholder="edit me"/>
-    <p>Multiline message is:</p>
-    <p style="white-space: pre-line;">{{ message }}</p>
-    <textarea v-model="message" placeholder="add multiple lines"></textarea>
+    <input type="checkbox" id="checkbox" v-model="checked" />
+    <label for="checkbox">{{ checked }}</label>
+
+    <div>Checked names: {{ checkedNames }}</div>
+
+    <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
+    <label for="jack">Jack</label>
+
+    <input type="checkbox" id="john" value="John" v-model="checkedNames">
+    <label for="john">John</label>
+
+    <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
+    <label for="mike">Mike</label>
 
 </template>
 
@@ -11,10 +19,10 @@
 export default {
     data() {
         return {
-            message: ' '
+            checked: false,
+            checkedNames: []
         }
-    },
-    methods: {}
+    }
 }
 </script>
 
