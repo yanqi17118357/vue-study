01a192bc9d71715f2f0fdcecf3f07e69ed86a0a3
yanqi17118357 <3224848830@qq.com>
2023-04-13 21:21:41 +0800

    练习-Attribute绑定

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,51 @@
-<script>
-import AlertBox from './AlertBox.vue'
+<!--
+现在我们将元素的 attribute / property 响应式地绑定到状态上。
+这个 :title 语法是 v-bind:title 的简写。
+-->
 
+<script>
 export default {
-    components: { AlertBox }
+    data() {
+        return {
+            message: 'Hello World!',
+            isRed: true,
+            color: 'green'
+        }
+    },
+    methods: {
+        toggleRed() {
+            this.isRed = !this.isRed
+        },
+        toggleColor() {
+            this.color = this.color === 'green' ? 'blue' : 'green'
+        }
+    }
 }
 </script>
 
 <template>
-    <AlertBox>
-        Something bad happened.
-    </AlertBox>
+    <p>
+    <span :title="message">
+      Hover your mouse over me for a few seconds to see my dynamically bound title!
+    </span>
+    </p>
+
+    <!--
+    除了普通字符串之外，
+    class 绑定还特别支持了对象和数组
+    -->
+    <p :class="{ red: isRed }" @click="toggleRed">
+        This should be red... but click me to toggle it.
+    </p>
+
+    <!-- 样式绑定也支持对象和数组 -->
+    <p :style="{ color }" @click="toggleColor">
+        This should be green, and should toggle between green and blue on click.
+    </p>
 </template>
+
+<style>
+.red {
+    color: red;
+}
+</style>
